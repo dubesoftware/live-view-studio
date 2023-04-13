1116795f9ed0ae72b43b5558ea8db0b6f5929b5c
dubesoftware <therealdubesoftware@gmail.com>
2023-04-13 23:07:46 +0200

    Fix module definition and define mount/3 callback

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -1,6 +1,12 @@
 defmodule LiveViewStudioWeb.LightLive do
-  
-  use Phoenix.LiveView
+  use LiveViewStudioWeb, :live_view
 
-  #  Do stuff here
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+    {:ok, socket}
+  end
+
+  # render
+
+  # handle_event
 end
